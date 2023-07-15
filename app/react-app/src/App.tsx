9e4b1837690f60da36dd2729d7f34715d383e84a
yuto ichinose <yuto.0526.sano@gmail.com>
2023-07-16 02:30:06 +0900

    first commit

---
+++ app/react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="circle">
+        <p>Hello React</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
